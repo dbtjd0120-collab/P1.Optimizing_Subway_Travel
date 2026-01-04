38dc6a3fbac1cff8e78d52e873dafde5837ee0dd
dbtjd0120-collab <dbtjd0120@gmail.com>
2026-01-04 20:33:57 +0900

    ..

--- practice_all_for_US.py
+++ practice_all_for_US.py
@@ -1,6 +1,27 @@
 #이유성 연습장
 #너희도 하나씩 만들어서 써
 
-a , b , c= map(int,input('숫자 3개를 입력하시오: ').split())
+f , s, t = map(int,input().split())
 
-print(a + b + c) 
+if f == s == t:
+    print(10000+f*1000)
+if f == s != t or f == t != s or s == t != f:
+    if f == s:
+        d = f
+    if f == t:
+        d = f
+    if s == t:
+        d = s
+    print(1000 + d*100)
+if f != s and s != t and f != t:
+    if f < s :
+        n = s
+    else:
+        n = f
+    if n < t:
+        n = t
+    print(100*n)
+        
+ 
+    
+   
